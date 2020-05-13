fb8bb4e1ae294aca037a9a08edf30bb047ff899f
Anthony Chu <antchu@microsoft.com>
2020-05-13 07:24:36 -0700

    Update to use deno links

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { AzureFunctionsWorker } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/master/mod.ts"
-export { Context as AzureFunctionsContext } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/master/types.ts";
+export { AzureFunctionsWorker } from "https://deno.land/x/azure_functions/mod.ts"
+export { Context as AzureFunctionsContext } from "https://deno.land/x/azure_functions/types.ts";
