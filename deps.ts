52b8c20a9c9df06d2cfb4f931490c5301b70789d
Anthony Chu <antchu@microsoft.com>
2020-05-13 01:47:07 -0700

    Update dependencies to GitHub

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { AzureFunctionsWorker } from "../../ms/azure-functions-deno-worker/mod.ts"
-export { Context as AzureFunctionsContext } from "../../ms/azure-functions-deno-worker/types.ts";
+export { AzureFunctionsWorker } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/master/mod.ts"
+export { Context as AzureFunctionsContext } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/master/types.ts";
