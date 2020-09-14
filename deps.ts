9c3546fa699aaa6aa68362bc7af15788769bf3e8
Anthony Chu <antchu@microsoft.com>
2020-09-14 00:31:30 -0700

    Update deps to preview

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { AzureFunctionsWorker } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/v0.5.0/mod.ts"
-export type { Context as AzureFunctionsContext } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/v0.5.0/types.ts";
+export { AzureFunctionsWorker } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/preview/mod.ts"
+export type { Context as AzureFunctionsContext } from "https://raw.githubusercontent.com/anthonychu/azure-functions-deno-worker/preview/types.ts";
